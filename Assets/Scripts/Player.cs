68a47e96bb3e671413576e3026a323e704e67ff1
mshr-h <mshr-h@users.noreply.github.com>
2015-12-29 18:08:15 +0900

    add player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 jumpForce = new Vector2(0, 300);
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Rigidbody2D>().AddForce(jumpForce);
+        }
+    }
+}
