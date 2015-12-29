d3d0a08191325e67f48179ee5f6c52c74f3c7a66
mshr-h <mshr-h@users.noreply.github.com>
2015-12-29 18:32:35 +0900

    put obstacle randomly

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class Obstacle : MonoBehaviour {
     public Vector2 velocity = new Vector2(-4, 0);
+    public float range = 4;
 
     void Start () {
         GetComponent<Rigidbody2D>().velocity = velocity;
+        GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y - range * Random.value, transform.position.z);
 	}
 }
