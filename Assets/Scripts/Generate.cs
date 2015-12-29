cae01fc7cf590bd9ce2c658de60bb28024ae6f27
mshr-h <mshr-h@users.noreply.github.com>
2015-12-29 18:32:17 +0900

    add obstacle generator

---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour {
+    public GameObject rocks;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("CreateObstacle", 1f, 1.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void CreateObstacle()
+    {
+        Instantiate(rocks);
+    }
+}
