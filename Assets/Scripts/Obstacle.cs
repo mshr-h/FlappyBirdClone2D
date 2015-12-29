db956f472237c51d5ec2060f17a710f488505ce1
mshr-h <mshr-h@users.noreply.github.com>
2015-12-29 18:15:55 +0900

    add obstacle

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+    public Vector2 velocity = new Vector2(-4, 0);
+
+    void Start () {
+        GetComponent<Rigidbody2D>().velocity = velocity;
+	}
+}
