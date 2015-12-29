dbb0c1f556432d406dd29837f6aa7158090e1e77
mshr-h <mshr-h@users.noreply.github.com>
2015-12-29 18:32:55 +0900

    add player die

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,5 +12,21 @@ public class Player : MonoBehaviour
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             GetComponent<Rigidbody2D>().AddForce(jumpForce);
         }
+
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        if (screenPosition.y > Screen.height || screenPosition.y < 0)
+        {
+            Die();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Die();
+    }
+
+    void Die()
+    {
+        Application.LoadLevel(Application.loadedLevel);
     }
 }
