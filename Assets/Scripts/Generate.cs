0e9c783c9d94d013be169d64d3be2c0b98f1605d
mshr-h <mshr-h@users.noreply.github.com>
2015-12-29 18:35:28 +0900

    keep score

--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -3,19 +3,22 @@ using System.Collections;
 
 public class Generate : MonoBehaviour {
     public GameObject rocks;
+    int score = 0;
 
     // Use this for initialization
     void Start () {
         InvokeRepeating("CreateObstacle", 1f, 1.5f);
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+        GUILayout.Label(" Score: " + score.ToString());
+    }
 
     void CreateObstacle()
     {
         Instantiate(rocks);
+        score++;
     }
 }
